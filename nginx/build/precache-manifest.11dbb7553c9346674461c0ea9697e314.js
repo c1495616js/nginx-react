54fa8b2c36d061728783e329ec658b2fbcf9cdb8
Jerry Wang <jerryappleid761208@gmail.com>
2019-09-25 14:00:11 -0700

    webpack-dev-server

---
+++ nginx/build/precache-manifest.11dbb7553c9346674461c0ea9697e314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9860372da65f99d910b9a812919c593d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5d639a777494dbc2621",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9ce300a323e0f775ac1f",
+    "url": "/static/js/2.1b2a34bd.chunk.js"
+  },
+  {
+    "revision": "d5d639a777494dbc2621",
+    "url": "/static/js/main.a0f47963.chunk.js"
+  },
+  {
+    "revision": "3bf3a7bc5a89176d157a",
+    "url": "/static/js/runtime-main.cb2ef8e0.js"
+  }
+]);
